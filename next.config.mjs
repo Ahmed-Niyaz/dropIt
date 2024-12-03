e3a0e794ced5e7726e79eda1d91b1c999536fd28
Niyaz Ahmed <niyazahmed@Niyazs-MacBook-Air.local>
2024-12-04 03:57:39 +0530

    added ignoreDuringbuilds true in next config

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
