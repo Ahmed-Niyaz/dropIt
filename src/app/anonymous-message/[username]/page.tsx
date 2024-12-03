c50becf290238b520f2493032501fd8b94d42ff5
Niyaz Ahmed <niyazahmed@Niyazs-MacBook-Air.local>
2024-12-04 03:41:55 +0530

    made changes in tsconfig

--- src/app/anonymous-message/[username]/page.tsx
+++ src/app/anonymous-message/[username]/page.tsx
@@ -56,9 +56,9 @@ export default function SendMessage() {
 
   const messageContent = form.watch("content");
 
-  const handleMessageClick = (message: string) => {
-    form.setValue("content", message);
-  };
+  // const handleMessageClick = (message: string) => {
+  //   form.setValue("content", message);
+  // };
 
   const [isLoading, setIsLoading] = useState(false);
   const [isAcceptingMsg, setIsAcceptingMsg] = useState(true);
